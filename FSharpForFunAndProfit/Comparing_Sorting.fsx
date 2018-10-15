4c65ca01a3e018eaa68eca7a4cbc863848100eef
Steven Law <stevlawonyou@yahoo.com>
2018-10-14 22:28:18 -0600

    Comparing Sorting and Comparing Webpages

--- FSharpForFunAndProfit/Comparing_Sorting.fsx
+++ FSharpForFunAndProfit/Comparing_Sorting.fsx
@@ -1,7 +1,61 @@
 ﻿// https://fsharpforfunandprofit.com/posts/fvsc-quicksort/
 let rec quicksort list =
     match list with
-    | [] ->
-        []
-    |
+    | [] ->                         // If the list is empty
+        []                          // return an empty list
+    | firstElem::otherElements ->   // If the list is not empty
+        let smallerElements =       // extract the smaller ones 
+            otherElements
+            |> List.filter (fun e -> e < firstElem)
+            |> quicksort            // and sort them
+        let largerElemnts =         // extract the large ones
+            otherElements
+            |> List.filter (fun e -> e >= firstElem)
+            |> quicksort            // and sort them
+        // Combine the 3 parts into a new list and return it
+        List.concat [smallerElements; [firstElem]; largerElemnts]
 
+//test
+printfn "%A" (quicksort [1;5;23;18;9;1;3])
+
+let rec quicksort2 = function
+    | [] -> []
+    | first::rest ->
+        let smaller, larger = List.partition ((>=) first) rest
+        List.concat [quicksort2 smaller; [first]; quicksort2 larger]
+
+//test 2
+printfn "%A" (quicksort2 [1;5;23;18;9;1;3])
+
+//public class QuickSortHelper
+//{
+//   public static List<T> QuickSort<T>(List<T> values) 
+//      where T : IComparable
+//   {
+//      if (values.Count == 0)
+//      {
+//         return new List<T>();
+//      }
+
+//      //get the first element       T firstElement = values[0];
+
+//      //get the smaller and larger elements       var smallerElements = new List<T>();
+//      var largerElements = new List<T>();
+//      for (int i = 1; i < values.Count; i++)  // i starts at 1       {                                       // not 0!          var elem = values[i];
+//         if (elem.CompareTo(firstElement) < 0)
+//         {
+//            smallerElements.Add(elem);
+//         }
+//         else
+//         {
+//            largerElements.Add(elem);
+//         }
+//      }
+
+//      //return the result       var result = new List<T>();
+//      result.AddRange(QuickSort(smallerElements.ToList()));
+//      result.Add(firstElement);
+//      result.AddRange(QuickSort(largerElements.ToList()));
+//      return result;
+//   }
+//}
---
+++ FSharpForFunAndProfit/Comparing_WebPage.fsx
@@ -0,0 +1,54 @@
+﻿open System.Net
+open System
+open System.IO
+
+let fetchUrl callback url =
+    let req = WebRequest.Create(Uri(url))
+    use resp = req.GetResponse()
+    use stream = resp.GetResponseStream()
+    use reader = new IO.StreamReader(stream)
+    callback reader url
+
+let myCallback (reader:IO.StreamReader) url =
+    let html = reader.ReadToEnd()
+    let html1000 = html.Substring(0, 1000)
+    printfn "Downloaded %s. First 1000 is %s" url html1000
+    html    // return all the html
+
+//test
+let google = fetchUrl myCallback "http://google.com"
+
+// build a function with the callback "baked in"
+let fetchUrl2 = fetchUrl myCallback
+
+// test
+let google2 = fetchUrl2 "http://www.google.com"
+let bbc      = fetchUrl2 "http://news.bbc.co.uk"
+
+// test with a list of sites
+let sites = ["http://www.bing.com";
+             "http://www.google.com";
+             "http://www.yahoo.com"]
+
+// process each site in the list
+sites |> List.map fetchUrl2
+
+//class WebPageDownloader
+//{
+//    public TResult FetchUrl<TResult>(
+//        string url,
+//        Func<string, StreamReader, TResult> callback)
+//    {
+//        var req = WebRequest.Create(url);
+//        using (var resp = req.GetResponse())
+//        {
+//            using (var stream = resp.GetResponseStream())
+//            {
+//                using (var reader = new StreamReader(stream))
+//                {
+//                    return callback(url, reader);
+//                }
+//            }
+//        }
+//    }
+//}
